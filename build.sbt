c0625f1eb18d3b14a20e90b5f6fc78694f6b393c
ikuo suzuki <isuzuki@ikuonoMacBook-Pro.local>
2017-10-24 04:58:19 +0900

    add endpoint test

--- build.sbt
+++ build.sbt
@@ -14,6 +14,7 @@ scalacOptions ++= Seq(
 val http4sVersion = "0.17.5"
 
 libraryDependencies ++= Seq(
-  "org.http4s" %% "http4s-blaze-server" % http4sVersion,
-  "org.http4s" %% "http4s-dsl"          % http4sVersion
+  "org.http4s"    %% "http4s-blaze-server" % http4sVersion,
+  "org.http4s"    %% "http4s-dsl"          % http4sVersion,
+  "org.scalatest" %% "scalatest"           % "3.0.4"        % "test"
 )
---
+++ src/test/scala/com/isuzuki/http4s/app/AppRouterTest.scala
@@ -0,0 +1,17 @@
+package com.isuzuki.http4s.app
+
+import org.http4s.{Method, Request}
+import org.http4s.dsl._
+import org.scalatest.{Matchers, WordSpec}
+
+class AppRouterTest extends WordSpec with Matchers {
+  "AppRouter" can {
+    "acccess hello path" should {
+      "return response status code 200" in {
+        val request = Request(Method.GET, uri("hello"))
+        val response = AppRouter.service.run(request).unsafeRunSync.right.get
+        response.orNotFound.status.code shouldBe 200
+      }
+    }
+  }
+}
