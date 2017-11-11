1a26831adeaac894400f0f0542bcb0ce6f6a19f5
isuzuki <ikkuntech@gmail.com>
2017-11-11 22:28:08 +0900

    rename app logger name (#3)

--- src/main/scala/com/isuzuki/http4s/app/package.scala
+++ src/main/scala/com/isuzuki/http4s/app/package.scala
@@ -3,5 +3,5 @@ package com.isuzuki.http4s
 import org.slf4j.{Logger, LoggerFactory}
 
 package object app {
-  lazy val appLogger: Logger = LoggerFactory.getLogger("AppLogger")
+  lazy val appLogger: Logger = LoggerFactory.getLogger("com.isuzuki.http4s.app.AppLogger")
 }
